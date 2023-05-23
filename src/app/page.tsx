76e70826ed9fbe3378d1a2c458be238dc129f439
Rahul Vig <rahulvig315@gmail.com>
2023-05-22 21:24:50 -0400

    style(app): game bg update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Game from "@/components/Game/Game";
 
 export default function Home() {
   return (
-    <main className="flex justify-center h-screen">
+    <main className="flex justify-center h-screen bg-black text-white">
       <Game />
     </main>
   )
